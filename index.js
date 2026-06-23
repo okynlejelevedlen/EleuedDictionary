ad82d2393f698f78639c63521195ecdf16d77513
Eli Overholtzer <elisover7@gmail.com>
2026-06-23 07:36:47 -0700

    Accusative beginning

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ function search() {
 
 function displayResults(results) {
     const resultDiv = document.getElementById("result");
+    const declineDiv = document.getElementById("decline");
 
     if (results.length === 0) {
         resultDiv.innerHTML = "<p>No matches found.</p>";
@@ -51,4 +52,72 @@ function displayResults(results) {
             <p>Related words: ${data.related.join(", ")}</p>
         </div>
     `).join("");
+
+    const { word, data } = results[0];
+
+    // Declension & Conjugation
+    switch(data.type) {
+        case "noun-frn": // Noun front with no vowel
+            declineDiv.innerHTML = results.map(({word, data}) => `
+                <h2>Declension</h2>
+                <table>
+                    <tr>
+                        <th>Case</th>
+                        <th>Singular</th>
+                        <th>Plural</th>
+                    </tr>
+                    <tr>
+                        <td>Nominative</td>
+                        <td>${word}</td>
+                        <td>${word + "en"}</td>
+                    </tr>
+                    <tr>
+                        <td>Accusative</td>
+                        <td>${accusative(word)}</td>
+                        <td>${accusative(word) + "en"}</td>
+                    </tr>
+                    <tr>
+                        <td>Genitive</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Dative</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Allative</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Ablative</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            `).join("");
+            break;
+        case "noun-frv":
+            break;
+        default: 
+            break;
+    }
+}
+
+function accusative(word) {
+    const vowels = "aeiouyöüë";
+
+    if (word.endsWith("dh") || word.endsWith("th")) {
+        return word + "h";
+    }
+
+    const lastLetter = word[word.length - 1];
+
+    if (vowels.includes(lastLetter)) {
+        return word + "h";
+    }
+
+    return word;
 }
